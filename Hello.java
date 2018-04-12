70143d88442f081ea169ef049b41108acbfbc71f
helloworld116 <646767424@qq.com>
2018-04-12 15:12:54 +0800

    另一个用户更新了

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
   public static void main(String args[]) {
     System.out.println("bugfix");
     System.out.println("bugfix too");
+    System.out.println("bugfix too");
   }
 }
