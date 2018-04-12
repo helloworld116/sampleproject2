42ea1a2a329d9001aa5bdb8fa5e8e587d9a31124
helloworld116 <646767424@qq.com>
2018-04-12 15:43:14 +0800

    change

--- Hello.java
+++ Hello.java
@@ -2,7 +2,5 @@ public class Hello {
   public static void main(String args[]) {
     System.out.println("patch");
     System.out.println("bugfix");
-    System.out.println("bugfix too");
-    System.out.println("bugfix too");
   }
 }
